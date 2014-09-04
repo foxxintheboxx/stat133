d1c80ab4a7aac33e1134d298abbd3b9c67a5b2ac
Ian Fox <ianfox41@gmail.com>
2014-09-04 16:02:18 -0700

    hw done

--- hw1/ex1.r
+++ hw1/ex1.r
@@ -26,7 +26,7 @@ two.plus.two <- 2 + 2
 
 # ex1.data <- your code here
 
-
+ex1.data = read.csv(file="ex1-data.csv",head=TRUE,sep=",")
 
 # The data you just loaded is stored as a data frame object. Your data frame
 # should have one column with the name "x". You can extract a column from a data
@@ -38,19 +38,19 @@ two.plus.two <- 2 + 2
 
 # ex1.data.v <- your code here
 
-
+ex1.data.v = ex1.data$x
 
 # find the class of <ex1.data.v>. Store this as a variable with the name
 # <data.class>
 
-# data.class <- your code here
+ data.class = class(ex1.data.v)
 
 
                     
 # create the variable <n.ex1.data> that gives the number of observations in the
 # vector <ex1.data.v>.
 
-# n.ex1.data <- your code here
+ n.ex1.data = ex1.data.v
 
 
 
@@ -58,45 +58,45 @@ two.plus.two <- 2 + 2
 # <data.mean>. In addition, calculate the trimmed mean, setting the trim
 # argument to 0.1. Store this as the variable <data.mean.trimmed>.
 
-# data.mean <- your code here
-# data.mean.trimmed <- your code here
+ data.mean = mean(ex1.data.v)
+ data.mean.trimmed = mean(ex1.data.v, trim = 0.1)
+
 
 
 
 # calculate the standard deviation of <ex1.data.v>. Store this as the variable
 # <data.sd>.
 
-# data.sd <- your code here
+ data.sd = sd(ex1.data.v, na.rm = FALSE)
 
 
 
 # find the minimum and maximum values of <ex1.data.v>. Store these as variables
 # with names <min.ex1.data> and <max.ex1.data> respectively.
 
-# min.ex1.data <- your code here
-# max.ex1.data <- your code here
-
+min.ex1.data = min(ex1.data.v)
+max.ex1.data = max(ex1.data.v)
 
 
 
 # use R's summary function to find the min, 1st quartile, median, mean, 3rd
 # quartile, and max of <ex1.data.v>. Store this as the variable <data.summary>.
 
-# data.summary <- your code here
+ data.summary = summary(ex1.data.v)
 
 
 
 # create the vector <ex1.data.double>. This should be a vector of each
 # observation from <ex1.data.v> doubled.
 
-# ex1.data.double <- your code here
+ex1.data.double = rep(ex1.data.v, each = 2)
 
 
 
 # calculate the sum of the observations in <ex1.data.v>. Store this as the
 # variable <ex1.data.sum>.
 
-# ex1.data.sum <- your code here
+ ex1.data.sum = sum(ex1.data.v)
 
 
 
@@ -106,7 +106,7 @@ two.plus.two <- 2 + 2
 # variable with the name <data.cor>
 
 set.seed(47) # THIS LINE MUST COME BEFORE YOUR GENERATED DATA. DO NOT REMOVE IT.
-# random.data <- your code here
-# data.cor <- your code here
+random.data = rnorm(length(ex1.data.v))
+data.cor = cor(ex1.data.v, random.data, use ="everything")
 
 
