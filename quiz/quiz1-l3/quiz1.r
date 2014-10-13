20124c6dcdafb84dc4135b5e6d998fd158c13d8d
Ian Fox <ianfox41@gmail.com>
2014-10-13 11:42:41 -0700

    quiz done

--- quiz/quiz1-l3/quiz1.r
+++ quiz/quiz1-l3/quiz1.r
@@ -1,13 +1,13 @@
 # Please load in the dataset included in the quiz1-l3 directory. It will be
 # required to perform the following tasks. The dataset includes data for countries in 2012.
 
-# your code here
+load("SummerOlympics2012Ctry.rda")
 
 # calculate the mean and the maximum of GDP in the dataset. Store these as the
 # variables <mean.GDP> and <max.GDP> respectively.
 
-# mean.GDP <- your code here
-# max.GDP <- your code here
+mean.GDP <- mean(SO2012Ctry$GDP)
+max.GDP <- max(SO2012Ctry$GDP)
 
 
 
@@ -16,7 +16,7 @@
 # <female.prop>. Note that this should be a numeric vector with length equal to
 # the number of observations in the dataset.
 
-# female.prop <- your code here
+female.prop <- SO2012Ctry$Female / (SO2012Ctry$Male + SO2012Ctry$Female)
 
 
 
@@ -26,8 +26,8 @@
 # 1) Countries with 0 bronze medals: <subset.nobronze>
 # 2) Countries with more than or exactly 3 bronze medals: <subset.threebronze>
 
-# subset.nobronze <- your code here
-# subset.threebronze <- your code here
+subset.nobronze <- SO2012Ctry[SO2012Ctry$Bronze == 0,]
+subset.threebronze <- SO2012Ctry[SO2012Ctry$Bronze == 3,]
 
 
 
@@ -35,8 +35,8 @@
 # For each of your subsets, create a vector giving the population size. Store
 # these as variables <subset.nobronze.pop> and <subset.threebronze.pop>.
 
-# subset.nobronze.pop <- your code here
-# subset.threebronze.pop <- your code here
+subset.nobronze.pop <- subset.nobronze[,"pop"]
+subset.threebronze.pop <- subset.threebronze[, "pop"]
 
 
 
@@ -55,7 +55,8 @@
 # whose values in <GDPPP> are strictly less that <GDPPP.cutoff>.
 
 medpopByGDPPP <- function(GDPPP.cutoff, GDPPP, pop){
-  # your code here  
+	indexes = which(GDPPP < GDPPP.cutoff)
+	median(pop[indexes])
 }
 
 
@@ -66,6 +67,6 @@ medpopByGDPPP <- function(GDPPP.cutoff, GDPPP, pop){
 # 2) axis labels: "Proportion of female athletes" and "Total # athletes"
 # 3) plotting character set to 10
 # 4) a red horizontal line at female proportion of 0.50.
-
-
+plot((SO2012Ctry$Male + SO2012Ctry$Female), female.prop, xlab = "Total # athletes", ylab = "Proportion of female athletes", main =  "Proportion of female athletes vs Total # athletes", pch = 10)
+abline(h = 0.5, col ="red")
 
