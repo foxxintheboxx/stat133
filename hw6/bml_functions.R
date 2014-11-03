5f6bcc70ec161bdf11715c8297ee8d1cef4845d5
Ian Fox <ianfox41@gmail.com>
2014-11-02 19:07:05 -0800

    indexes to advance function works, move.car func seems to work and so does step

--- hw6/bml_functions.R
+++ hw6/bml_functions.R
@@ -7,11 +7,12 @@
 ## Output : A matrix [m] with entries 0 (no cars) 1 (red cars) or 2 (blue cars)
 ## that stores the state of the system (i.e. location of red and blue cars)
 
-bml.init <- function(r, c, p){
-
-  
-   #return(m)
+bml.initl<- function(r, c, p) { 
+   m <- matrix(rbinom(r*c,c(1,2),p),nrow=r,ncol=c)
+   m
 }
+
+
 
 #### Function to move the system one step (east and north)
 ## Input : a matrix [m] of the same type as the output from bml.init()
@@ -23,8 +24,85 @@ bml.init <- function(r, c, p){
 
 bml.step <- function(m){
 
-  
-   #return(list(m, grid.new))
+  #advance north the
+	new.m = apply(m, 2, function(c) { move.car(c, 1) })
+	for (i in 1:nrow(new.m)) {
+	 	new.m[i,] = move.car(new.m[i,], 2)
+	}
+
+	new.m
+}
+move.car <- function(vector, value) {
+	  	indexes = indexes.to.advance(vector,value)
+	  	print(value)
+	  	sequence = 1:length(vector)
+	  	if (value == 2)
+	  	{
+		 	sequence = rev(sequence) 	
+	  	}
+  		for (i in sequence){
+  			if (i %in% indexes)
+  			{
+
+  				if (value == 1)
+  				{
+	  				new.i = i - 1
+	  			} else {
+	  				new.i = (i + 1) %% length(vector)
+	  			}
+  				if (new.i == 0) {
+  					new.i = length(vector)
+  				}
+  				print(new.i)
+  				vector[new.i] = vector[i]
+  				vector[i] = 0
+  			}
+  		}
+  		if (value == 2)
+	  	{
+	  		print("return")
+	  		print(vector)
+	  		
+	  	}
+	  	vector
+}
+
+#WORKING
+indexes.to.advance <- function(vector, v)
+{
+	all.indexes = NULL
+	sublist = NULL
+	sequence = 1:length(vector)
+	if (v == 1) {
+		sequence = rev(sequence)
+	}
+	for (curr in sequence) {
+		prev = NULL
+		if (v == 1)
+		{
+			prev <- (curr - 1)%% (length(vector))
+			if (prev == 0) {
+				prev = length(vector)
+			}
+		} else {
+			prev <- (curr + 1)%% (length(vector) + 1)
+			if (prev == 0) {
+				prev = 1
+			}
+		}
+
+		if ((vector[prev] == 0 || prev %in% all.indexes) && vector[curr] == v) {
+
+			all.indexes = c(all.indexes, sublist, curr)
+			sublist = NULL
+		} else if (vector[curr] == v) {
+
+			sublist = c(sublist, curr)
+		} else if (vector[curr] != 0) {
+			sublist = NULL
+		}
+	}
+	all.indexes
 }
 
 #### Function to do a simulation for a given set of input parameters
